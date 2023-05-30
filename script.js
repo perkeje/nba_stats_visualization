bb9dd3be9336b8eb6b7b0c68b384206f3a002d66
Juraj Peric <juraj.peric@gmail.com>
2023-05-30 22:58:48 +0200

    Added graph that shows progress through years

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var westZoom = d3.select(".west-zoom-btn");
 var rstZoom = d3.select(".rst-zoom-btn");
 var intervalId;
 var hoveredTeam = null;
+let selectedTeam = null;
 var criteria = null;
 const baseScale = 1200;
 const baseTranslateX = 480;
@@ -61,6 +62,47 @@ var zoom = d3
     .on("zoom", (event) => {
         g.attr("transform", event.transform);
     });
+
+const linePlotMargin = { top: 20, right: 20, bottom: 30, left: 40 };
+const linePlotWidth = 600;
+const linePlotHeight = 400;
+
+const linePlotSvg = d3
+    .select(".line-plot")
+    .append("svg")
+    .attr("width", linePlotWidth + linePlotMargin.left + linePlotMargin.right)
+    .attr("height", linePlotHeight + linePlotMargin.top + linePlotMargin.bottom)
+    .append("g")
+    .attr(
+        "transform",
+        `translate(${linePlotMargin.left},${linePlotMargin.top})`
+    );
+
+let xAxisLabel = linePlotSvg
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("x", linePlotWidth / 2)
+    .attr("y", linePlotHeight + linePlotMargin.bottom)
+    .style("text-anchor", "middle")
+    .text("Year")
+    .style("fill", "#fefcfb");
+
+let yAxisLabel = linePlotSvg
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -linePlotHeight / 2)
+    .attr("y", -linePlotMargin.left + 11)
+    .style("text-anchor", "middle")
+    .style("fill", "#fefcfb");
+
+let plotTitle = linePlotSvg
+    .append("text")
+    .attr("class", "plot-title")
+    .attr("x", linePlotWidth / 2)
+    .attr("y", -5)
+    .style("text-anchor", "middle")
+    .style("fill", "#fefcfb");
 
 let mapInteraction = async () => {
     try {
@@ -149,6 +191,7 @@ let mapInteraction = async () => {
 };
 
 const createCircles = (year, teams) => {
+    const teamChartData = [];
     let circles = g.selectAll("circle").data(
         teams.filter((d) => {
             return d.Year === year;
@@ -208,9 +251,33 @@ const createCircles = (year, teams) => {
                 (d) => `${radiusScale(d[criteria])}px`
             );
         })
+        .on("click", (e, d) => {
+            selectedTeam = d.team_id;
+            const teamData = teams.filter(
+                (team) => team.team_id === selectedTeam && team.Year <= year
+            );
+            const teamChartData = teamData.map((data) => ({
+                Year: data.Year,
+                Value: +data[criteria],
+            }));
+            plotTitle.text(`Team: ${d.name}`);
+            renderLinePlot(teamChartData);
+        })
         .transition()
         .duration(500)
         .attr("r", (d) => `${radiusScale(d[criteria])}px`);
+
+    if (selectedTeam) {
+        const teamData = teams.filter(
+            (team) => team.team_id === selectedTeam && team.Year <= year
+        );
+        const teamChartData = teamData.map((data) => ({
+            Year: data.Year,
+            Value: +data[criteria],
+        }));
+
+        renderLinePlot(teamChartData);
+    }
 
     if (hoveredTeam) {
         const teamData = teams.find(
@@ -249,6 +316,7 @@ const createPatterns = (teams) => {
 
 const setSelectedCriteria = () => {
     criteria = d3.select("input[name='criteria']:checked").node().value;
+    yAxisLabel.text(criteria);
 };
 
 d3.select(".teams-div").on("mousemove", function (e, d) {
@@ -258,4 +326,115 @@ d3.select(".teams-div").on("mousemove", function (e, d) {
         .style("top", currentMousePosition[1] + "px");
 });
 
+function initializeLinePlot() {
+    const xScale = d3.scaleBand().range([0, linePlotWidth]).padding(0.1);
+
+    const yScale = d3.scaleLinear().range([linePlotHeight, 0]);
+
+    const xAxis = d3
+        .axisBottom(xScale)
+        .tickValues(xScale.domain().filter((d, i) => i % 5 === 0));
+
+    const yAxis = d3.axisLeft(yScale);
+
+    linePlotSvg
+        .append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0, ${linePlotHeight})`)
+        .style("fill", "#1282a2")
+        .call(xAxis);
+
+    linePlotSvg
+        .append("g")
+        .attr("class", "y-axis")
+        .style("fill", "#1282a2")
+        .call(yAxis);
+
+    linePlotSvg
+        .append("rect")
+        .attr("width", linePlotWidth)
+        .attr("height", linePlotHeight)
+        .style("fill", "#fefcfb");
+}
+
+function renderLinePlot(data) {
+    const xScale = d3
+        .scaleBand()
+        .domain(data.map((d) => d.Year))
+        .range([0, linePlotWidth])
+        .padding(0.1);
+
+    const yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, (d) => d.Value)])
+        .range([linePlotHeight, 0]);
+
+    const xAxis = d3
+        .axisBottom(xScale)
+        .tickValues(xScale.domain().filter((d, i) => i % 5 === 0));
+
+    const yAxis = d3.axisLeft(yScale);
+
+    linePlotSvg.select(".x-axis").transition().duration(500).call(xAxis);
+
+    linePlotSvg.select(".y-axis").transition().duration(500).call(yAxis);
+
+    const line = d3
+        .line()
+        .x((d) => xScale(d.Year))
+        .y((d) => yScale(d.Value));
+
+    const path = linePlotSvg.selectAll(".line").data([data]);
+
+    path.enter()
+        .append("path")
+        .attr("class", "line")
+        .style("stroke", "red")
+        .style("fill", "none")
+        .merge(path)
+        .transition()
+        .duration(500)
+        .attr("d", line);
+
+    path.exit().remove();
+
+    const circles = linePlotSvg
+        .selectAll(".circle-year")
+        .data(data, (d) => d.Year);
+
+    circles.exit().transition().duration(500).attr("r", 0).remove();
+
+    const newCircles = circles
+        .enter()
+        .append("circle")
+        .attr("class", "circle-year")
+        .attr("cx", (d) => xScale(d.Year))
+        .attr("cy", (d) => yScale(d.Value))
+        .attr("r", 0)
+        .style("fill", "red")
+        .style("stroke", "white")
+        .style("stroke-width", 2)
+        .style("cursor", "pointer");
+
+    circles
+        .merge(newCircles)
+        .on("mouseover", (e, d) => {
+            tooltip
+                .html(`Year: ${d.Year}<br>Value: ${d.Value}`)
+                .style("opacity", 0.8)
+                .style("left", `${d3.pointer(e)[0]}px`)
+                .style("top", `${d3.pointer(e)[1]}px`);
+        })
+        .on("mouseout", () => {
+            tooltip.style("opacity", 0);
+        })
+        .transition()
+        .duration(500)
+        .attr("cx", (d) => xScale(d.Year))
+        .attr("cy", (d) => yScale(d.Value))
+        .attr("r", 4);
+}
+
+setSelectedCriteria();
 mapInteraction();
+initializeLinePlot();
